7b827123f04308d9562f9d2d541133c474d2150c
abdofer <abderrazaq.fermaq@gmail.com>
2023-03-16 17:47:19 -0700

    Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - A program that prints a line with puts function
- * Return 0 (success)
- */
+* main - A program that prints a line with puts function
+*
+* Return: Always 0 (success)
+*/
 int main (void)
 {
 	char a;
