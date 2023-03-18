34d2df24bfe85eb7b9e9c65f70c7096708ca0613
abdofer <abderrazaq.fermaq@gmail.com>
2023-03-18 15:12:11 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,37 +1,22 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there /
 #include <stdio.h>
-/ betty style doc for function main goes there*/
+
 /**
- * main - prints all the numbers of base 16 in lowercase.
+ * main - prints numbers between 0 to 9 and letters a to f.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int a=0;
-int b;
+	int i;
 
-while (a <= 99)
-{
-        b = a + 1;
-    while (b <= 99)
-    {
-        putchar (a / 10 + '0');
-        putchar (a % 10 + '0');
-        putchar (' ');
-        putchar (b / 10 + '0');
-        putchar (b % 10 + '0');
-        if (a != 98)
-        {
-	       	putchar(',');
-		putchar(' ');
-        }
-        b++;
-    }
-    a++;
-}
-return (0);
-
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
