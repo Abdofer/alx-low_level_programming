413e0c0268eff812a6dfcf1996ce12f1da8876cd
abdofer <abderrazaq.fermaq@gmail.com>
2023-03-18 16:11:46 -0700

    Inventing is a combination of brains and material

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include <time.h>
+
+/**
+* main - Inventing is a combination of brains and material
+*
+* Description: using the main function
+* this program prints "Inventing is a combination of brains and material"
+* Return: 0
+*/
+int main(void)
+{
+	int n, m;
+	
+	for (n = 48; n <= 56; n++)
+	{
+		for (n = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(' ');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
