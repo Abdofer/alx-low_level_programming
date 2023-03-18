d90a698f4f44b010f46d2e3f6ba0334fd8a24711
abdofer <abderrazaq.fermaq@gmail.com>
2023-03-17 19:46:26 -0700

    ffff

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * main base 10 starting from 0
- *
+ * main - base 10 starting from 0
+ * Description: using the main function
+ * this program prints "base 10 starting from 0"
  * Return: Always 0 (Success)
  */
 int main(void)
