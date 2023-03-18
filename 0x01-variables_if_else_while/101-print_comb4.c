865d22e53f6a74caabbc75592ef213537f966cdb
abdofer <abderrazaq.fermaq@gmail.com>
2023-03-18 16:59:25 -0700

    possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,29 +6,27 @@
  */
 int main(void)
 {
-    int num1, num2, num3;
-
-    for (num1 = 48; num1 <= 57; num1++)
-    {
-        for (num2 = 48; num2 <= 57; num2++)
-        {
-            for (num3 = 48; num3 <= 57; num3++)
-            {
-                if (num1 < num2)
-                {
-                    putchar(num1);
-                    putchar(num2);
-                    putchar(num3);
-                    if (num2 != 57 || num1 != 56 || num3 != 55)
-                    {
-                        putchar(44);
-                        putchar(32);
-                    }
-                }
-            }
-        }
-    }
-    putchar('\n');
-
-    return (0);
+	int num1, num2, num3;
+	for (num1 = 48; num1 <= 57; num1++)
+	{
+		for (num2 = 48; num2 <= 57; num2++)
+		{
+			for (num3 = 48; num3 <= 57; num3++)
+			{
+				if (num1 < num2)
+				{
+					putchar(num1);
+					putchar(num2);
+					putchar(num3);
+					if (num2 != 57 || num1 != 56 || num3 != 55)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
