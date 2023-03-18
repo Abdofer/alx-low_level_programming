1be27c985decddca4ef638218348ad9b98c0106e
abdofer <abderrazaq.fermaq@gmail.com>
2023-03-17 19:02:18 -0700

    base 10 starting from

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 30; i < 40; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
