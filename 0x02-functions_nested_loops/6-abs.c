789f88003736e3124f361c742101952f618425f5
abdofer <abderrazaq.fermaq@gmail.com>
2023-03-21 19:09:48 -0700

    absolute value in this world

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
 /**
- * _abs - computes the absolute value of an integer
- * @n: the int to check
- * Return: the absolute value of int
- */
+   *_abs - returns the absolute value of a number
+   *@n: character to be checked
+   *
+   *Return: This function returns a postive int number
+   */
+
 int _abs(int n)
 {
 	if (n >= 0)
 	{
-		return (0);
+		return (n);
 	}
-	return (-n);
+	else
+	{
+		return (n * -1);
+	}
 }
